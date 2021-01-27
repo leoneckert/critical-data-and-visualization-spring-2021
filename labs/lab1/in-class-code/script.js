7286371dbbf3b32330af096e695965217cf417db
leoneckert <leoneckert@mailbox.org>
2021-01-27 11:19:40 +0800

    lab1 code

--- labs/lab1/in-class-code/script.js
+++ labs/lab1/in-class-code/script.js
@@ -1,50 +1,9 @@
-## Lab 1 - collecting dataset
+// heavily commented version of what we did in class today
 
-[Quick Link to **How to collect data using Google Forms**](../collect-data-google-form)
+// find instructions on how to get the data here:
+// https://github.com/leoneckert/critical-data-and-visualization-spring-2021/tree/main/labs/collect-data-google-form
 
-#### todays agenda:
-- [DOM, what is it?](#slides)
-- [how a browser meets a website](#slides)
-- [how a browser sees html](#slides)
-- [css and js, endless metaphors](#slides)
-- [review homework](#review-homework)
-- [review JavaScript data structures](#review-javascript-data-structures)
-- [Collect data with Google Forms](#collect-data-with-google-forms)
-- [Transform Data](#transform-data)
-- [Mini data visualization using JavaScript](#mini-data-visualization-using-javascript)
-
-### slides
-
-Here are the [slides](https://docs.google.com/presentation/d/1dgxYXxxQO4_PeyDetlwsARiS_HWgBY-FcvcbhxUUJGY/edit?usp=sharing) from today that include **browser, meet website**, **how a browser sees html** and **css and js, endless metaphors**
-
-### review homework
-a strategy to solve the exercise from this weeks Coding Foundation is:
-1. link css and js
-2. create box manually (html & css)
-3. recreate box with js (createElement, className, appendChild)
-4. create box on button click (function, getElementById, addEventListener)
-5. create three boxes on button click (for loop)
-6. read value of input (getElementById, value)
-
-
-
-### review JavaScript data structures
-[Here are slides](https://docs.google.com/presentation/d/1C7FiumimDZhSFaILnOj50oYgr7kE8_jRzD-bepwFHgw/edit?usp=sharing) I drew to explain JavaScript data structures.
-
-(if you feel confident, you can skip this part)
-
-It ends with [this exercise](http://cdv.leoneckert.com/json-nav) on navigating a JavaScript object. Let's try it.
-
-### Collect data with Google Forms
-
-[Here is a tutorial](../collect-data-google-form) guiding you through using [Google Forms](https://docs.google.com/forms/) to collect data and then turn it into a format that is easy to work with using [this trick](http://blog.pamelafox.org/2013/06/exporting-google-spreadsheet-as-json.html).
-
-Today, we will all use the same dataset collected using this method. It is here for you to **copy and paste** (will update in class):
-
-```json
-
-
-[
+let data = [
     {
         "timestamp": "2021-01-27T02:48:02.529Z",
         "watermelonWithFetaCheese": 1,
@@ -168,112 +127,106 @@ Today, we will all use the same dataset collected using this method. It is here 
 ]
 
 
-```
 
-### Transform Data
+// A function Leon wrote to transform the structure of the data
+// The function with many comments on description of what kind of
+// data it transform into what other kind of data is here:
+// https://github.com/leoneckert/critical-data-and-visualization-spring-2021/tree/master/labs/lab1#transform-data
 
-Below is a function to transform the dataset we get from the Google Sheet. It's heavily commented for better understanding.
 
-```javascript
-// the function dates a data
-// arrayn as an argument
 function averageData(data){
-  // new empty array to be filled
-  // with data in new structure
   let newData = [];
-  // assuming each data point has the same
-  // keys/categories, we extract an array of them from the
-  // first data point in the array
-  // in class we changed it to the last element instead
-  // as the first one did not have all the categories filled out
-  // there is more thorough ways to do this, but for out purposes
-  // now, this will be enough
-  let keys = Object.keys(data[0]);
-  // now we loop over the keys/categories
+  let keys = Object.keys(data[ data.length-1 ]);
   for(let i = 0; i < keys.length; i++){
-    // store the current key/category in
-    // a variable:
     let key = keys[i];
-    // now we will loop over each data point
-    // in the data set, check if it has a value
-    // for the key/category and add them to
-    // a total sum variable
-    // as well as count the occurences in order to
-    // calulate the averae in the end
     let sum = 0;
     let num = 0;
     for(let j = 0; j < data.length; j++){
       let datum = data[j];
-      // check if the key exists
-      // for this datapoint
       if(key in datum){
-        // add to sum
         sum += datum[key];
-        // increase count
         num++;
       }
     }
-    // now calculate the average
     let avg = sum/num;
-    // make sure the value is a number
-    // (some value might be strings)
     if(!isNaN(avg)){
-      // create an object with both the average
-      // and also the number of measurements that
-      // went into the average
       let newDataPoint = {"name": key, "average": avg, 'numMeasurements': num};
-      // add the new datapoint to the new data array
       newData.push(newDataPoint);
     }
   }
-  // return everything when it is done
   return newData;
 }
-```
 
-Example what this function does. If a dataset is:
-```json
-data = [
-    {
-        "category1": 3,
-        "category2": 7,
-
-    },
-    {
-        "category1": 2,
-        "category2": 2,
-    },
-
-]
-```
-then, after using the function like
-```javascript
+// here we use the function to transform the data
 let transformedData = averageData(data);
-```
-this
-```javascript
+
+
+// log things to the console as you go to make sure things
+// are behaving as intended. I see the data looks good
 console.log(transformedData);
-```
-will return
-```json
-[
-    {
-        "name": "category1",
-        "average": 2.5,
-        "numMeasurements": 2,
 
-    },̨
-    {
-        "name": "category2",
-        "average": 4.5,
-        "numMeasurements": 2,
-    },
+// up here, we also select the container (div) on the page
+// into which we insert the bars we create in the loop below
+let container = document.getElementById("container");
 
-]
-```
 
-### Mini data visualization using JavaScript
+// Loop over the data, once for each datapoint.
+// in our case the loop loops 5 times because we have
+// 5 foods in out dataset.
+for(let i = 0; i < transformedData.length; i++){
+  // get the item we deal with at this iteration
+  let datapoint = transformedData[i];
+  // datapoint looks like this in the first iteration:
+  // {
+  //   average: 2.066666666666667
+  //   name: "watermelonWithFetaCheese"
+  //   numMeasurements: 15
+  // }
 
-![our plan](assets/plan.png)
+  // get the name of the current
+  //  item by "digging" into the datapoint object
+  // and the average liking value
+  let food = datapoint.name;
+  let average = datapoint.average;
 
-We code together in class to achieve this.
+  // in each iteration (once for each food)
+  // we create a div
+  let bar = document.createElement("div");
+
+  // next, we assign the className to the div
+  // that will make sure basic bar styling (defined in css/style.css)
+  // is applied to it (like height, color, margin)
+  bar.className = "bar";
+  // because the width is different for each bar
+  // we define it right here in javascript using the
+  // average value of each data point that we iterate over
+  // we stored average value in the variable "average" on line 178
+  bar.style.width = (average * 20) + "px";
+
+
+  // labels
+  // so far we have a div that has a width that corresponds
+  // to the data value. next we create a p-tag the
+  // text (innerHTML) of which is the name of the food currently
+  // iterated over
+  // we stored the name in the variable "food" on line 184
+  // create the tag
+  let barname = document.createElement("p");
+  // change the text
+  barname.innerHTML = food;
+  // now here we insert the p tag into the div tag we created above
+  bar.appendChild(barname);
+  // at this point the element we have created using JavaScript only
+  // looks like:
+  // <div class="bar"><p>watermelonWithFetaCheese</p></div>
+  // (that is for the first datapoint (watermelonWithFetaCheese) that we iterate over)
+
+  // that whole element (the div containing the p tag)
+  // we append to the body after all
+  // bring it from "JavaScript world" to "HTML world"
+  container.appendChild(bar);
+
+
+
+
+}
